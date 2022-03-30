8bec1da0f8c528ec3ca359cec17ac270731b9c4a
Chris Adams <chris@cadams.com.au>
2022-03-30 11:18:10 +1100

    Do not launch other browsers

--- route-up.sh
+++ route-up.sh
@@ -9,15 +9,19 @@ darwin_close_tabs() {
     # See https://stackoverflow.com/questions/12358270/closing-specific-tab-in-firefox-using-applescript
     for browser in "Google Chrome" "Safari"; do
         osascript <<EOF
-        tell application "${browser}"
-            set windowList to every tab of every window whose URL starts with "http://127.0.0.1:35001"
-            repeat with tabList in windowList
-                set tabList to tabList as any
-                repeat with tabItr in tabList
-                    set tabItr to tabItr as any
-                    delete tabItr
-                end repeat
-            end repeat
+        tell application "System Events"
+            if (get name of every application process) contains "${browser}" then
+                tell application "${browser}"
+                    set windowList to every tab of every window whose URL starts with "http://127.0.0.1:35001"
+                    repeat with tabList in windowList
+                        set tabList to tabList as any
+                        repeat with tabItr in tabList
+                            set tabItr to tabItr as any
+                            delete tabItr
+                        end repeat
+                    end repeat
+                end tell
+            end if
         end tell
 EOF
     done
